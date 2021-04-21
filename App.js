f4ca13e7600a92721d862be7edba0be2d3b0ed19
mathtkang <ksge1124@gmail.com>
2021-04-21 19:15:39 +0900

    styles1)Displaying Temperature

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from 'expo-location';
 import axios from "axios";
+import Weather from "./Weather";
+// window.React = React;
 
 const API_KEY = "58b146b03deca0a09bbced4fc195f4af";
 
@@ -12,22 +14,19 @@ export default class extends React.Component {
   };
   getWeather = async (latitude, longitude) => {
     const { data } = await axios.get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`
     );
-    console.log(data);
+    // console.log(data);
+    this.setState({ isLoading : false, temp : data.main.temp });
   };
   getLocation = async () => {
     try {
-      // const response = await Location.requestPermissionsAsync(); //이제는 비추천하는 메소드
-      // console.log(response);
       await Location.getBackgroundPermissionsAsync();
       const { 
         coords: {latitude, longitude} 
       } = await Location.getCurrentPositionAsync();
-      // console.log(coords.latitude, coords.longitude);
-      // // Send to API and get weather!
       this.getWeather(latitude, longitude)
-      this.setState({ isLoading : false });
+      // this.setState({ isLoading : false });
     } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
@@ -36,7 +35,7 @@ export default class extends React.Component {
     this.getLocation();
   }
   render() {
-    const { isLoading } = this.state;
-    return isLoading ? <Loading /> : null;
-  }
+    const { isLoading, temp } = this.state;
+    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} />;
+  };
 }
